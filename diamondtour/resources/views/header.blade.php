9fb00656fc7789dd7ebe2dd92642bc295ae935ab
Phạm Lộc <locpdt@etrustcloud.com>
2023-11-13 22:54:25 +0700

    Updated Homepage

---
+++ diamondtour/resources/views/header.blade.php
@@ -0,0 +1,34 @@
+<header>
+  <div class="box-header">
+    <!-- Mobile Header -->
+    <div class="wsmobileheader clearfix ">
+      <a id="wsnavtoggle" class="wsanimated-arrow"><span></span></a>
+      <span class="smllogo"><a href="#"><img src="{{ asset('assets/images/logo.png') }}" width="80" alt=""/></a></span>
+    </div>
+    <!-- Mobile Header -->
+    <div class="container">
+      <div class="block-header-main--wrap d-flex flex-row w-100 h-100 align-items-center justify-content-between">
+        <div class="block-header-main--logo">
+          <a href="#"><img src="{{ asset('assets/images/logo.png') }}" alt="" class="img-fluid"></a>
+        </div>
+        <div class="block-header-main--menu">
+          <div class="wsmainwp clearfix">
+            <nav class="wsmenu clearfix">
+              <ul class="wsmenu-list">
+                <li><a class="active" href="#">HOME</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Destinations</a></li>
+                <li><a href="#">Tours</a></li>
+                <li><a href="#">Library</a></li>
+                <li><a href="#">Blogs</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="block-header-main--navigation">
+          <!-- <a href="#"><i class="fa-solid fa-bars"></i></a> -->
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
