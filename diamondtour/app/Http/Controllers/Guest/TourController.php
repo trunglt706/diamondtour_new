30a3f988126c90e39cdc4174dea24460e7ec0c1e
Lâm Thành Trung <trunglt@cfox.vn>
2024-01-14 21:59:26 +0700

    trunglt update

---
+++ diamondtour/app/Http/Controllers/Guest/TourController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+
+class TourController extends Controller
+{
+
+    public function index()
+    {
+    }
+
+    public function details($alias)
+    {
+    }
+}
