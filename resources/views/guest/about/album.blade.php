9e5ed1254111b35d85ef182d1b36077f472564f2
trunglt <trunglt@cfox.vn>
2025-11-13 16:14:04 +0700

    test get file from s3 aws

--- resources/views/guest/about/album.blade.php
+++ resources/views/guest/about/album.blade.php
@@ -6,8 +6,8 @@
         <div class="list-image">
             @foreach ($about_images as $item)
                 <div class="img">
-                    <a data-fancybox="gallery" data-src="{{ asset($item->url) }}">
-                        <img src="{{ asset('style/images/blank.jpg') }}" data-src="{{ asset($item->url) }}" alt="Image" srcset="" loading="lazy">
+                    <a data-fancybox="gallery" data-src="{{ get_file($item->url) }}">
+                        <img src="{{ asset('style/images/blank.jpg') }}" data-src="{{ get_file($item->url) }}" alt="Image" srcset="" loading="lazy">
                     </a>
                 </div>
             @endforeach
