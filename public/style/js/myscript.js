834e56d033c4b3f9723792dff3c4cd036174d48e
trunglt <trunglt@cfox.vn>
2025-11-13 23:59:05 +0700

    update js

--- public/style/js/myscript.js
+++ public/style/js/myscript.js
@@ -2,60 +2,59 @@ $(document).ready(function () {
     $(window).resize(function () {
         let width = $(window).width();
         if (width >= 991 && width <= 1366) {
-            $('#box-container').removeClass('container');
-            $('#box-container').addClass('container-fluid');
+            $("#box-container").removeClass("container");
+            $("#box-container").addClass("container-fluid");
         } else {
-            $('#box-container').removeClass('container-fluid');
-            $('#box-container').addClass('container');
+            $("#box-container").removeClass("container-fluid");
+            $("#box-container").addClass("container");
         }
     });
 
-    Fancybox.bind('[data-fancybox]', {
-        l10n: Fancybox.l10n.de
+    Fancybox.bind("[data-fancybox]", {
+        l10n: Fancybox.l10n.de,
     });
-});
 
-$(function () {
-    $('input[name="daterange"]').daterangepicker({
-        opens: "left",
-    },
-        function (start, end, label) {
+    $('input[name="daterange"]').daterangepicker(
+        {
+            opens: "left",
+        },
+        function (start, end) {
             const startDate = start.format("YYYY-MM-DD");
             const endDate = end.format("YYYY-MM-DD");
             const url = `/search?t=tour&start=${startDate}&end=${endDate}`;
             location.href = url;
         }
     );
-});
 
-const $lazyImages = $('img[loading="lazy"][data-src]');
+    const $lazyImages = $('img[loading="lazy"][data-src]');
 
-function loadImage($img) {
-    const src = $img.data('src');
-    if (!src) return;
+    function loadImage($img) {
+        const src = $img.data("src");
+        if (!src) return;
 
-    const highRes = new Image();
-    highRes.src = src;
-    highRes.onload = function () {
-        $img.attr('src', src).addClass('loaded');
-    };
+        const highRes = new Image();
+        highRes.src = src;
+        highRes.onload = function () {
+            $img.attr("src", src).addClass("loaded");
+        };
 
-    $img.removeAttr('data-src'); // tránh load lại
-}
+        $img.removeAttr("data-src"); // tránh load lại
+    }
 
-function lazyLoad() {
-    $lazyImages.each(function () {
-        const $img = $(this);
-        loadImage($img);
+    function lazyLoad() {
+        $lazyImages.each(function () {
+            const $img = $(this);
+            loadImage($img);
+        });
+    }
+
+    // Gọi 1 lần đầu
+    lazyLoad();
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "bottom-end",
+        showConfirmButton: false,
+        timer: 3000,
     });
-}
-
-// Gọi 1 lần đầu
-lazyLoad();
-
-const Toast = Swal.mixin({
-    toast: true,
-    position: "bottom-end",
-    showConfirmButton: false,
-    timer: 3000,
 });
